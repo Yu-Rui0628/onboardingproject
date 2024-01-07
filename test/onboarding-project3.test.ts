eb55f0a6243e9fd0f5da57879c4e4af6ded4e2c9
RuiYu <ruyu4807@uni.sydney.edu.au>
2024-01-08 04:12:14 +1100

    Initial commit

---
+++ test/onboarding-project3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OnboardingProject3 from '../lib/onboarding-project3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/onboarding-project3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OnboardingProject3.OnboardingProject3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
